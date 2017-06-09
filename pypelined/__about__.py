bfe2e58cec524c6fe7b32e7832f97579c8cb1fda
Max Fischer <maxfischer2781@gmail.com>
2017-06-09 17:30:51 +0200

    metadata update

--- pypelined/__about__.py
+++ pypelined/__about__.py
@@ -2,7 +2,7 @@ __title__ = 'pypelined'
 __summary__ = 'stream and pipeline processing service'
 __url__ = 'https://github.com/maxfischer2781/pypelined'
 
-__version__ = '0.1.dev3'
+__version__ = '0.1.0'
 __author__ = 'Max Fischer'
-__email__ = 'max.fischer@kit.edu'
-__copyright__ = '2016 - 2017 %s' % __author__
+__email__ = 'maxfischer2781@gmail.com'
+__copyright__ = '2017 %s' % __author__
