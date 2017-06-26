c24a4c3329d2b10d85b6460d33edb6b81755b63d
Max Fischer <maxfischer2781@gmail.com>
2017-06-26 18:30:07 +0200

    added missing dependencies

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
         url=package_about['__url__'],
         packages=find_packages(),
         # dependencies
-        install_requires=['apmon', 'filelock'],
+        install_requires=['apmon', 'filelock', 'chainlet', 'include'],
         # metadata for package search
         license='MIT',
         # https://pypi.python.org/pypi?%3Aaction=list_classifiers
